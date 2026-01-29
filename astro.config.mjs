e9b5b9548788f120ed664f590e2cff768041e47e
Wojtek <wkolodziej09@hotmail.com>
2026-01-29 22:35:27 +0100

    xd

--- astro.config.mjs
+++ astro.config.mjs
@@ -41,7 +41,7 @@ export default defineConfig({
   image: {
     service: {
       entrypoint: "astro/assets/services/noop",
-    }
+    },
   },
   adapter: cloudflare({
     imageService: "compile",
